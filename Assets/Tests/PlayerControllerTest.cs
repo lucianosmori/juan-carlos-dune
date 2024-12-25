6f56343ecb4a8f5df52f76fce0292b6782fa20e9
Cloudbroker <smluciano@gmail.com>
2024-12-25 14:23:42 -0500

    rename unit tests

--- Assets/Tests/PlayerControllerTest.cs
+++ Assets/Tests/PlayerControllerTest.cs
@@ -3,11 +3,11 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class NewTestScript
+public class PlayerControllerTest
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void NewTestScriptSimplePasses()
+    public void PlayerControllerTestSimplePasses()
     {
         // Use the Assert class to test conditions
     }
@@ -15,7 +15,7 @@ public class NewTestScript
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    public IEnumerator PlayerControllerTestWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
